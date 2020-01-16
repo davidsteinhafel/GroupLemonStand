de714f8f1dddb5d5fa1871f43cea9618c9208f01
David steinhafel <davidsteinhafel@outlook.com>
2020-01-16 09:31:56 -0600

    Added player class with member variables and a name in constructor

--- Lemonade Stand/Player.cs
+++ Lemonade Stand/Player.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace Lemonade_Stand
 {
     class Player
+        //possibly abstract class?
     {
+        public string name;
+        public Inventory inventory;
+        public Wallet wallet;
+        public Recipe recipe;
+        public Pitcher pitcher;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
     }
+    
 }
