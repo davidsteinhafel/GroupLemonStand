6cd13e8d6a2799b3001f3d7891036194fc7a85a3
David steinhafel <davidsteinhafel@outlook.com>
2020-01-16 13:45:00 -0600

    added set name method for player

--- Lemonade Stand/Recipe.cs
+++ Lemonade Stand/Recipe.cs
@@ -13,9 +13,28 @@ namespace Lemonade_Stand
         public int amountOfIceCubes;
         public double pricePerCup;
 
+        
+
         public Recipe()
         {
+            amountOfLemons = 4;
+            amountOfIceCubes = 4;
+            amountOfSugarCubes = 4;
+            pricePerCup = .25;
+
+            this.amountOfIceCubes = amountOfIceCubes;
+            this.amountOfLemons = amountOfLemons;
+            this.amountOfSugarCubes = amountOfSugarCubes;
+            this.pricePerCup = pricePerCup;
+
+            new Pitcher pitcher1();
             //Constructor
+        }
+        public void MakeLemonade()
+        {
+            amountOfSugarCubes + amountOfLemons + amountOfIceCubes = new Pitcher();
+            //lemons + sugar + iceCube 
+            //4lemon + 4cups + 4 ice cub
         }
     }
 }
