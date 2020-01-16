3c75d67d8fbca3376bc22e18de2d0d688df33051
David steinhafel <davidsteinhafel@outlook.com>
2020-01-16 10:28:31 -0600

    added lemon,sugarcube,icecube, and cup classes. Added customer, and recipe class

--- Lemonade Stand/Recipe.cs
+++ Lemonade Stand/Recipe.cs
@@ -8,5 +8,14 @@ namespace Lemonade_Stand
 {
     class Recipe
     {
+        public int amountOfLemons;
+        public int amountOfSugarCubes;
+        public int amountOfIceCubes;
+        public double pricePerCup;
+
+        public Recipe()
+        {
+            //Constructor
+        }
     }
 }
